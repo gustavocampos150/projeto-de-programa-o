99b08851ff3ea781b476f1b39a08a5ac8ec5b64c
gustavocampos150 <gustavo.camp9s66@gmail.com>
2024-10-21 07:09:06 -0300

    Add files via upload

---
+++ perguntas.js
@@ -0,0 +1,23 @@
+criaCartao(
+    'metal',
+    'O que é heavy metal?',
+    'é um subgênero do rock com guitarras distorcidas e batidas pesadas '
+)
+
+criaCartao(
+    'metal',
+    'oque é deth metal?',
+    'andamento acelerado, vocais guturais, blast beats e letras que abordam a morte, horror, violência e anti-religião'
+)
+
+criaCartao(
+    'rock',
+    'O que é o hard rock?',
+    'ondas sonoras em alta voltagem, com muita potência, porém com ritmos e arranjos simples.'
+)
+
+criaCartao(
+    'banda',
+    'banda famosa',
+    'Nirvana foi uma banda norte-americana de rock formada pelo vocalista e guitarrista Kurt Cobain e pelo baixista Krist Novoselic em Aberdeen no ano de 1987,[1] que obteve grande sucesso no movimento grunge de Seattle no início dos anos 1990'
+)
